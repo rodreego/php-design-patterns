25158badc028690e5daa6fdd63fbe66485441e37
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2016-10-26 13:57:24 -0200

    psr-1

--- patterns/creational/singleton/Singleton.php
+++ patterns/creational/singleton/Singleton.php
@@ -2,27 +2,35 @@
 
 namespace patterns\creational\singleton;
 
-class Singleton{
+class Singleton
+{
 
 	private static $singleton = null;
 	public $state;
-
-	private function __construct(){
+	/**
+	* Classes que tenham o construtor privado não podem gerar objetos.
+	* No entanto, aqui faz sentido, uma vez que se trata de uma classe Singleton
+	*/
+	private function __construct()
+	{
 		$this->setState('Initial');
 	}
 
-	public static function getInstance(){
-		if(self::$singleton == null){
+	public static function getInstance()
+	{
+		if (self::$singleton == null) {
 			self::$singleton = new Singleton();
 		}
 		return self::$singleton;
 	}
 
-	public function getState(){
+	public function getState()
+	{
 		return $this->state;
 	}
 
-	public function setState($state){
+	public function setState($state)
+	{
 		$this->state = $state;
-	}	
+	}
 }
