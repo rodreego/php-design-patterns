bc3b7f390d182ff5f5bc8cb8d167b3e4cbca2aa2
Rodrigo Nascimento <rodrigocesarnascimento@gmail.com>
2016-10-26 13:59:10 -0200

    --amend

--- patterns/creational/singleton/ContextoDeSeguranca.php
+++ patterns/creational/singleton/ContextoDeSeguranca.php
@@ -2,14 +2,19 @@
 
 namespace patterns\creational\singleton;
 
-class ContextoDeSeguranca{
+class ContextoDeSeguranca
+{
 
 	private static $contextoDeSeguranca = null;
 
-	private function __construct() {}
+	private function __construct()
+	{
 
-	public static function getInstance(){
-		if(self::$contextoDeSeguranca === null){
+	}
+
+	public static function getInstance()
+	{
+		if (self::$contextoDeSeguranca === null) {
 			self::$contextoDeSeguranca = new ContextoDeSeguranca();
 		}
 		return self::$contextoDeSeguranca;
